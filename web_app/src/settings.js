24c846dd25feee83ad579fae1b33c2f3e4b9a886
fredy8 <fredy.altamirano8@gmail.com>
2016-08-24 01:01:15 -0500

    update web_app settings

--- web_app/src/settings.js
+++ web_app/src/settings.js
@@ -1,5 +1,19 @@
-export default {
+import servers from 'servers';
+
+let settings = {
   api: {
-    host: `https://@@PROD_SERVER_HOSTNAME@@:@@API_PORT@@/`
+    host: `https://${servers.production.hostname}:@@API_PORT@@/`
   }
 };
+
+if (process.env.NODE_ENV !== 'production') {
+  const devSettings = {
+    api: {
+      host: `http://127.0.0.1:@@API_PORT_DEV@@/`
+    }
+  };
+
+  settings = R.merge(settings, devSettings);
+}
+
+export default settings;
